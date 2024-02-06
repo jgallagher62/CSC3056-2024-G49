24498d7b2887b7ac81fc3bac6bd73674e7d8b2f0
lauramccool <lauramccool@localhost>
2024-02-06 18:14:37 +0000

    Updating Transaction Class

--- Lab1/src/model/Transaction.java
+++ Lab1/src/model/Transaction.java
@@ -1,5 +1,49 @@
 package model;
 
-public class Transaction {
+import java.util.Date;
 
+public class Transaction {
+	String accountNumber;
+	double transactionAmount;
+	Date transactionDate;
+	
+	
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	
+	
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	
+	
+	public double getTransactionAmount() {
+		return transactionAmount;
+	}
+	
+	
+	public void setTransactionAmount(double transactionAmount) {
+		this.transactionAmount = transactionAmount;
+	}
+	
+	
+	public Date getTransactionDate() {
+		return transactionDate;
+	}
+	
+	
+	public void setTransactionDate(Date transactionDate) {
+		this.transactionDate = transactionDate;
+	}
+
+
+	public Transaction(String accountNumber, double transactionAmount, Date transactionDate) {
+		super();
+		this.accountNumber = accountNumber;
+		this.transactionAmount = transactionAmount;
+		this.transactionDate = transactionDate;
+	}
+	
+	
 }
