9545a04244c41e206076337ffe0c7a5708dda4f8
lauramccool <lauramccool@localhost>
2024-02-27 18:00:18 +0000

    Range JUnit Test

--- Lab2/src/org/jfree/data/test/RangeTest.java
+++ Lab2/src/org/jfree/data/test/RangeTest.java
@@ -2,23 +2,22 @@ package org.jfree.data.test;
 
 import static org.junit.Assert.*;
 
+import org.jfree.data.Range;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class RangeTest {
+	
+	private Range rangeObjectUnderTest;
 
 	@Before
 	public void setUp() throws Exception {
+		rangeObjectUnderTest = new Range(-1, 1);
 	}
 
 	@After
 	public void tearDown() throws Exception {
-	}
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
 	}
 
 }
